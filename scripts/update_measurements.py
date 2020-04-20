8eef0b0a083c0ad81ca14157b1f17f4458daa0fc
Elías Gago <egago@icarto.es>
2020-04-20 21:29:49 +0200

    Script to generate random measurements from previously exported json file

---
+++ scripts/update_measurements.py
@@ -0,0 +1,18 @@
+import json
+from random import randrange
+
+
+with open("/home/elias/Proyectos/aecid/test/lecturas.json") as f:
+    data = json.load(f)
+
+updated_measurements = []
+for measurement in data:
+    measurement["lectura"] = (
+        measurement["lectura_anterior"] + randrange(40)
+        if measurement["cuota_fija"] != 2.72
+        else measurement["lectura_anterior"]
+    )
+    updated_measurements.append(measurement)
+
+with open("/home/elias/Proyectos/aecid/test/lecturas_updated.json", "w") as json_file:
+    json.dump(updated_measurements, json_file)

af959d567c8510f6e408e22b188a313cdf8e2e9b
Francisco Puga <fpuga@icarto.es>
2023-08-18 13:49:08 +0200

    Includes coverage plugin for pytest

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,4 @@
 # set -o pipefail:  fail the whole pipeline on first error
 set -euo pipefail
 
-pytest --log-cli-level=Warning back
+pytest --log-cli-level=Warning back --cov=back --cov-report=term-missing:skip-covered --cov-branch --no-cov-on-fail --cov-report html

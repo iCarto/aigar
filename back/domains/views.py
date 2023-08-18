328ef7df6aa89ddf411786c88c01bac3e880f2da
Francisco Puga <fpuga@icarto.es>
2023-08-18 13:51:10 +0200

    Adds a cache for zone viewset

--- back/domains/views.py
+++ back/domains/views.py
@@ -1,3 +1,5 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -7,8 +9,12 @@ from domains.models.zone import Zone
 from domains.serializers import ZoneSerializer
 
 
+TWO_HOURS = 60 * 60 * 2
+
+
+@method_decorator(cache_page(TWO_HOURS), name="dispatch")
 class ZoneViewSet(viewsets.ReadOnlyModelViewSet):
-    """API endpoint that allows Zones to be viewed or edited."""
+    """API endpoint that allows Zones to be viewed."""
 
     queryset = Zone.objects.all()
     serializer_class = ZoneSerializer

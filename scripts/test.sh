161b35037192b83919d6df46b175797f812acafb
Francisco Puga <fpuga@icarto.es>
2023-08-21 18:41:22 +0200

    Improves test infraestructure
    * Adds a generic conftest file and root level
    * Uses strict marks
    * Workaround to execute tests with real database data

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,7 @@
 # set -o pipefail:  fail the whole pipeline on first error
 set -euo pipefail
 
-pytest --log-cli-level=Warning back --cov=back --cov-report=term-missing:skip-covered --cov-branch --no-cov-on-fail --cov-report html
+pytest --log-cli-level=Warning back --cov=back --cov-report=term-missing:skip-covered --cov-branch --no-cov-on-fail --cov-report html -m "not slow"
+if [[ -d back/back/tests/api_real ]]; then
+    pytest --log-cli-level=Warning back/back/tests/api_real
+fi

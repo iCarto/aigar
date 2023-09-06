6d0cd4d81f39f2ec08c92625f4334fbf1eb72dca
Celia Martín <cmartin@icarto.es>
2023-09-06 13:12:12 +0200

    Add id_pago to payment object & get id_factura from DB in front adapter

--- front/src/payment/model/Payment.js
+++ front/src/payment/model/Payment.js
@@ -6,12 +6,14 @@ const payment_api_adapter = payment => {
     return {
         id_factura: payment["id_factura"],
         fecha: DateUtil.parse(payment["fecha"]),
-        monto: payment["monto"],
+        monto: parseFloat(payment["monto"]),
     };
 };
 
 const payment_front_adapter = payment => {
     payment["fecha"] = DateUtil.format(payment["fecha"]);
+    payment["id_factura"] = payment["factura"];
+
     return payment;
 };
 
@@ -25,17 +27,22 @@ const createPayments = (data = []) => {
 };
 
 const createPayment = ({
-    id = -1,
-    sector = -1,
-    num_socio = -1,
-    nombre_socio = "",
-    num_factura = "",
-    id_factura = -1,
+    id_pago = -1,
     fecha = -1,
     monto = -1,
+    num_socio = -1,
+    nombre_socio = "",
+    sector = -1,
+    num_factura = "",
+    id_factura = -1,
     errors = [],
 } = {}) => {
     const publicApi = {
+        // id_pago es el id que llega del back y se guarda en BD. Las propiedades fecha y monto también están en BD; el resto de campos solo se usan en el front.
+        id_pago,
+        fecha,
+        monto,
+        // Este id se genera para controlar la edición del nº factura o fecha en la tabla de importación de pagos (LoadPaymentsStep2PaymentsTable). No está en BD.
         id: num_factura + fecha,
         num_socio:
             num_socio !== -1 ? parseInt(num_socio) : parseInt(num_factura.substr(0, 4)),
@@ -43,8 +50,6 @@ const createPayment = ({
         sector,
         num_factura: num_factura !== "" ? num_factura.padStart(12, "0") : num_factura,
         id_factura,
-        fecha,
-        monto: parseFloat(monto),
         errors,
     };
 

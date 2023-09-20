b61ecc3e0461894348b1fe4cccfd49f9c73351b9
Francisco Puga <fpuga@icarto.es>
2023-09-20 16:03:52 +0200

    Improves start.sh script
    Now is prepared to launch the front or the back with custom options

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,48 @@
 #!/bin/bash
-set -e
 
-# honcho -f Procfile.dev start
-# exit
+set -euo pipefail
+
+function show_help() {
+    echo "Ayuda"
+}
+
+usage() {
+    echo "Syntax: $(basename "${0}") [-f] [-b]"
+    echo "Options:"
+    echo "-f | --front : Launches frontend"
+    echo "-b | --back : Launches backend"
+    exit 1
+}
+
+die() {
+    printf '%s\n' "${1}" >&2
+    exit 1
+}
+
+if [[ "${#}" -gt 1 ]]; then
+    die "ERROR: Only one option can be given"
+fi
+
+while [[ -n "$1" ]]; do
+    case "$1" in
+        -h | -\? | --help)
+            usage
+            ;;
+        -f | --front)
+            # https://coderrocketfuel.com/article/stop-create-react-app-from-opening-a-browser-window-on-start
+            (cd front && BROWSER=none REACT_APP_API_BASE_URL=http://localhost:8000 npm run start)
+            ;;
+        -b | --back)
+            # if ! command -v deactivate ; then
+            #     die "ERROR: Activate a virtualenv before continue."
+            # fi
+            # command -v deactivate || die "ERROR: Activate a virtualenv before continue."
+            (cd back && python manage.py runserver)
+            ;;
+        *)
+            die "ERROR: Unknown option: ${1}"
+            ;;
+    esac
+
+    shift
+done

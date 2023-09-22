9ef1666c5bc54f12731641472ceaee4fbe0ffb87
Francisco Puga <fpuga@icarto.es>
2023-09-22 21:31:44 +0200

    Removes User and Group from Backoffice

--- back/back/admin/__init__.py
+++ back/back/admin/__init__.py
@@ -1,8 +1,11 @@
 from django.conf.locale.es import formats as es_formats
-from django.contrib import admin
+from django.contrib import admin, auth
 
 from .member_admin import MemberAdmin
 
+
+admin.site.unregister(auth.models.User)
+admin.site.unregister((auth.models.Group))
 
 es_formats.DATETIME_FORMAT = "d/m/Y H:i:s"
 

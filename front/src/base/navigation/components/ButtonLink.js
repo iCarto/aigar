194ee52a1762d18473f221ad28fb06d3692b63c4
Celia Martín <cmartin@icarto.es>
2023-10-03 16:59:14 +0200

    Make hover for ButtonLink consistent with Button

--- front/src/base/navigation/components/ButtonLink.js
+++ front/src/base/navigation/components/ButtonLink.js
@@ -2,13 +2,20 @@ import {Link} from "react-router-dom";
 import Button from "@mui/material/Button";
 
 const ButtonLink = ({text, to, disabled = false}) => {
+    const buttonStyle = {
+        textAlign: "center",
+        "&a:hover": {
+            color: "inherit",
+        },
+    };
+
     return (
         <Button
             variant="contained"
             component={Link}
             to={to}
             disabled={disabled}
-            sx={{textAlign: "center"}}
+            sx={buttonStyle}
             fullWidth
         >
             {text}

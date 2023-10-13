990b953a8e0d917be5c05fd2f5a49bf943635f49
Celiamf <celiamartinfern@gmail.com>
2023-10-13 12:04:21 +0200

    Update conditions to disable update payments and print invoices buttons

--- front/src/monthlyinvoicing/container/ListMonthlyInvoicesSidebar.js
+++ front/src/monthlyinvoicing/container/ListMonthlyInvoicesSidebar.js
@@ -28,6 +28,7 @@ const ListMonthlyInvoicesSidebar = ({
     const outputFilename = `recibo_${selectedInvoicingMonth.anho}_${selectedInvoicingMonth.mes}_todos`;
 
     const isStartInvoicingDisabled = !isNewMonthSelected;
+
     /*
         Botón IMPORTAR LECTURAS deshabilitado cuando:
         * No haya facturas para ese mes (que sería el caso de que has cambiado el selector de mes y no has iniciado la facturación)
@@ -38,26 +39,55 @@ const ListMonthlyInvoicesSidebar = ({
         !invoices?.length ||
         invoices?.every(invoice => invoice.caudal_actual) ||
         invoices?.some(invoice => invoice.estado !== ESTADOS_FACTURA.NUEVA);
+
     /*
         Botón IMPRIMIR FACTURAS deshabilitado cuando:
         * No haya facturas para ese mes
         * o alguna factura no tenga caudal_actual o es 0. Es decir no se ha importado la lectura para alguna.
+        * o alguna factura tienen estado = pendiente de cobro (es decir, que ya se han impreso)
     */
     const isPrintInvoicesButtonDisabled =
-        !invoices?.length || invoices?.some(invoice => !invoice.caudal_actual);
+        !invoices?.length ||
+        invoices?.some(invoice => !invoice.caudal_actual) ||
+        invoices?.some(
+            invoice => invoice.estado === ESTADOS_FACTURA.PENDIENTE_DE_COBRO
+        );
+
     /*
         Botón ACTUALIZAR PAGOS deshabilitado cuando:
         * No haya facturas para ese mes
-        * o todas las facturas están cobradas
+        * o haya al menos una factura con estado = nueva
+        * o todas las facturas sean estado = (cobrada o no cobrada)
     */
+    // TO-DO: ¿No podría ser que esté deshabilitado cuando no haya ninguna factura PENDIENTE? Esto es lo mismo que si todas las facturas están pagadas o cobradas, y salvamos el caso de que haya alguna factura anulada, por ejemplo.
     const isLoadPaymentsButtonDisabled =
         !invoices?.length ||
-        invoices?.every(invoice => invoice.estado === ESTADOS_FACTURA.COBRADA);
+        invoices?.some(invoice => invoice.estado === ESTADOS_FACTURA.NUEVA) ||
+        invoices?.every(
+            invoice =>
+                invoice.estado === ESTADOS_FACTURA.COBRADA ||
+                invoice.estado === ESTADOS_FACTURA.NO_COBRADA
+        );
+    // || !invoices?.some(
+    //     invoice => invoice.estado === ESTADOS_FACTURA.PENDIENTE_DE_COBRO
+    // );
 
+    /*
+        Sabemos que el proceso de facturación ha terminado cuando:
+        * el botón de imprimir facturas está desactivado (ya se han impreso)
+        * Y no hay ninguna factura nueva ni no cobrada (porque entonces estaríamos en el punto 1 o 2 del proceso, iniciar facturación o importar lecturas)
+        * PERO ADEMÁS no todas están pendientes de cobro (porque entonces significa que los pagos aún no se han actualizado)
+    */
     const isMonthlyInvoicingDone =
-        isLoadMeasurementsButtonDisabled &&
         isPrintInvoicesButtonDisabled &&
-        isLoadPaymentsButtonDisabled;
+        !invoices?.some(
+            invoice =>
+                invoice.estado === ESTADOS_FACTURA.NUEVA ||
+                invoice.estado === ESTADOS_FACTURA.NO_COBRADA
+        ) &&
+        invoices?.some(
+            invoice => invoice.estado !== ESTADOS_FACTURA.PENDIENTE_DE_COBRO
+        );
 
     const isNextMonthButtonDisabled =
         isCurrentInvoicingMonth && !isMonthlyInvoicingDone;

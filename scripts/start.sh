d121355f95eedaaadc98ace9200a2a22d862379f
Francisco Puga <fpuga@icarto.es>
2023-10-16 19:40:23 +0200

    Upgrades selenium and adds stubs for drf

--- scripts/start.sh
+++ scripts/start.sh
@@ -23,29 +23,25 @@ if [[ "${#}" -gt 1 ]]; then
     die "ERROR: Only one option can be given"
 fi
 
-while [[ -n "$1" ]]; do
-    case "$1" in
-        -h | -\? | --help)
-            usage
-            ;;
-        -f | --front)
-            # https://coderrocketfuel.com/article/stop-create-react-app-from-opening-a-browser-window-on-start
-            (cd front && BROWSER=none REACT_APP_API_BASE_URL=http://localhost:8000 npm run start)
-            ;;
-        -b | --back)
-            # if ! command -v deactivate ; then
-            #     die "ERROR: Activate a virtualenv before continue."
-            # fi
-            # command -v deactivate || die "ERROR: Activate a virtualenv before continue."
-            (cd back && python manage.py runserver)
-            ;;
-        -s | --shell)
-            (cd back && python manage.py shell_plus --ipython)
-            ;;
-        *)
-            die "ERROR: Unknown option: ${1}"
-            ;;
-    esac
-
-    shift
-done
+case "${1}" in
+    -h | -\? | --help)
+        usage
+        ;;
+    -f | --front)
+        # https://coderrocketfuel.com/article/stop-create-react-app-from-opening-a-browser-window-on-start
+        (cd front && BROWSER=none REACT_APP_API_BASE_URL=http://localhost:8000 npm run start)
+        ;;
+    -b | --back)
+        # if ! command -v deactivate ; then
+        #     die "ERROR: Activate a virtualenv before continue."
+        # fi
+        # command -v deactivate || die "ERROR: Activate a virtualenv before continue."
+        (cd back && python manage.py runserver)
+        ;;
+    -s | --shell)
+        (cd back && python manage.py shell_plus --ipython)
+        ;;
+    *)
+        die "ERROR: Unknown option: ${1}"
+        ;;
+esac

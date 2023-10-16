34ebece3fbf1c5578eadd61b043d63f4be12b6b4
Celiamf <celiamartinfern@gmail.com>
2023-10-16 16:52:36 +0200

    Remove unused components

--- front/src/base/form/index.js
+++ front/src/base/form/index.js
@@ -4,5 +4,4 @@ export {default as FormInputDecimal} from "./FormInputDecimal";
 export {default as FormInputInteger} from "./FormInputInteger";
 export {default as FormSelect} from "./FormSelect";
 export {default as FormTextArea} from "./FormTextArea";
-export {default as FormLabel} from "./FormLabel";
 export {default as FormSelectOrder} from "./FormSelectOrder";

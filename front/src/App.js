09956c0540082121cfa036b217a80d85324d059a
Celiamf <celiamartinfern@gmail.com>
2023-10-18 12:33:31 +0200

    Add date & localization adapters to App

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,25 @@
-import {AigarRoutes} from "aigar/routes";
-import {theme} from "./Theme";
+import moment from "moment";
+import "moment/locale/es";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import CssBaseline from "@mui/material/CssBaseline";
+import {AdapterMoment} from "@mui/x-date-pickers/AdapterMoment";
+import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
+import {AigarRoutes} from "aigar/routes";
+import {theme} from "./Theme";
 
 function App() {
     return (
         <ThemeProvider theme={theme}>
-            <CssBaseline>
-                <div className="App">
-                    <AigarRoutes />
-                </div>
-            </CssBaseline>
+            <LocalizationProvider
+                dateAdapter={AdapterMoment}
+                adapterLocale={moment.locale("es")}
+            >
+                <CssBaseline>
+                    <div className="App">
+                        <AigarRoutes />
+                    </div>
+                </CssBaseline>
+            </LocalizationProvider>
         </ThemeProvider>
     );
 }

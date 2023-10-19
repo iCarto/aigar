74711e91fe025a1777a7a21059d3506d2d3e3010
Celiamf <celiamartinfern@gmail.com>
2023-10-19 08:51:26 +0200

    Update Stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,14 +2,9 @@ module.exports = {
     defaultSeverity: "error",
 
     plugins: ["stylelint-order"],
-    extends: "stylelint-config-standard",
+    extends: ["stylelint-config-standard", "stylelint-config-html"],
     rules: {
         "order/properties-alphabetical-order": true,
-        indentation: [4, {baseIndentLevel: 1}],
-        linebreaks: "unix",
-        "max-line-length": 88,
-        "no-empty-first-line": true,
-        "string-quotes": ["double", {avoidEscape: true}],
         "font-family-name-quotes": "always-unless-keyword",
         "function-url-quotes": "always",
         "selector-attribute-quotes": "always",
@@ -17,7 +12,6 @@ module.exports = {
         "no-descending-specificity": null,
 
         "function-calc-no-unspaced-operator": true,
-        "selector-pseudo-class-parentheses-space-inside": "never",
     },
 
     // https://stylelint.io/user-guide/configuration/#ignorefiles

45e108ffaf354fe192623ce0e2bcc5263c8b7a13
Francisco Puga <fpuga@icarto.es>
2024-04-02 19:09:47 +0200

    New version: 24.4.2

--- front/src/aigar/config/index.js
+++ front/src/aigar/config/index.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = "23.12.05";
+export const APP_VERSION = "24.4.2";
 export const APP_NAME = "Aigar";
 export const APP_NAME_LONG = "Aplicación innovadora para la gestión de agua rural";
 export const APP_LOGO_URL = "/logo/logo_aigar.png";
